e7acbec875d762f79fe09e7bc2144d74797282ef
PrenitaVulpo <guilhermepdl97@protonmail.com>
2021-06-16 15:29:30 -0300

    lifecycle test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,27 @@
 <template>
-  <div class="home"></div>
+	<div class="home"></div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: "Home",
-  components: {},
+	name: "Home",
+	components: {},
+	setup() {
+		console.log("setup");
+	},
+	beforeCreate() {
+		console.log("creating");
+	},
+	created() {
+		console.log("created");
+	},
+	beforeMount() {
+		console.log("mounting");
+	},
+	mounted() {
+		console.log("mounted");
+	},
 });
 </script>
