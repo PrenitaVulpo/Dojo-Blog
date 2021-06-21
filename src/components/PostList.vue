e463041be43f2d19c5816dddea8b110b757e46d7
PrenitaVulpo <guilhermepdl97@protonmail.com>
2021-06-21 15:29:54 -0300

    add singlepost component

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,16 +1,20 @@
 <template>
 	<div class="post-list">
 		<div v-for="post in posts" :key="post.id">
-			<h1>{{ post.title }}</h1>
+			<SinglePost :post="post" />
 		</div>
 	</div>
 </template>
 
 <script lang="ts">
-import Post from "../TS/Interfaces/Post";
 import { defineComponent, PropType } from "vue";
+import Post from "../TS/Interfaces/Post";
+import SinglePost from "@/components/SinglePost.vue";
 
 export default defineComponent({
+	components: {
+		SinglePost,
+	},
 	props: {
 		posts: {
 			type: Array as PropType<Array<Post>>,
---
+++ src/components/SinglePost.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="post">
+		<h1>{{ post.title }}</h1>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import Post from "@/TS/Interfaces/Post";
+
+export default defineComponent({
+	props: {
+		post: {
+			type: Object as PropType<Post>,
+			required: true,
+		},
+	},
+	// setup() {},
+});
+</script>
+
+<style></style>
