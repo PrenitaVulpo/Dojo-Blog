0aebd53f7ff89a14033fc91cb6bcb4fc1db73e06
PrenitaVulpo <guilhermepdl97@protonmail.com>
2021-06-21 14:13:35 -0300

    add postlist component

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,7 +1,23 @@
-<template></template>
+<template>
+	<div class="post-list">
+		<div v-for="post in posts" :key="post.id">
+			<h1>{{ post.title }}</h1>
+		</div>
+	</div>
+</template>
 
-<script>
-export default {};
+<script lang="ts">
+import Post from "../TS/Interfaces/Post";
+import { defineComponent, PropType } from "vue";
+
+export default defineComponent({
+	props: {
+		posts: {
+			type: Array as PropType<Array<Post>>,
+			required: true,
+		},
+	},
+});
 </script>
 
 <style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,17 @@
 <template>
 	<div class="home"></div>
+	<PostList :posts="posts" />
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from "vue";
+import PostList from "../components/PostList.vue";
 
 export default defineComponent({
 	name: "Home",
-	components: {},
+	components: {
+		PostList,
+	},
 	setup() {
 		const posts = ref([
 			{ title: "welcome to the blog", body: "lorem ipsum", id: 1 },
