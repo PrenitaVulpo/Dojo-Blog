a86c04b78ca7a9043a3c4682b3e0b5f950d40cfe
PrenitaVulpo <guilhermepdl97@protonmail.com>
2021-06-22 22:10:22 -0300

    add rotation to spinner

--- src/components/Spinner.vue
+++ src/components/Spinner.vue
@@ -11,5 +11,11 @@
 	border: 3px solid transparent;
 	border-radius: 50%;
 	border-top-color: #ff8800;
+	animation: spin 1s ease infinite;
+}
+@keyframes spin {
+	to {
+		transform: rotateZ(360deg);
+	}
 }
 </style>
