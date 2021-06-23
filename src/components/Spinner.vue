891d9386fdb9dc1e55c2724ec74f9d83d82176c3
PrenitaVulpo <guilhermepdl97@protonmail.com>
2021-06-22 22:00:56 -0300

    add spinner

---
+++ src/components/Spinner.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="spin" />
+</template>
+
+<style>
+.spin {
+	display: block;
+	width: 2.5rem;
+	height: 2.5rem;
+	margin: 1.875 rem auto;
+	border-radius: 50%;
+	border-top-color: #ff8800;
+}
+</style>
